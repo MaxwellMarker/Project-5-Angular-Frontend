b43cf2b575ee550cb5b814bbbd7a041e7c5f1844
Max Marker <maxmarker98@gmail.com>
2020-09-30 00:37:14 -0400

    fixed the loading error in the details component

--- src/app/game-details/game-details.component.ts
+++ src/app/game-details/game-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import { Location, CommonModule } from '@angular/common';
 
 import { Game } from '../game';
 import { GameService } from '../game.service';
@@ -11,23 +11,23 @@ import { GameService } from '../game.service';
   styleUrls: ['./game-details.component.scss']
 })
 export class GameDetailsComponent implements OnInit {
-
+  
   game: Game;
-
+  
+  ngOnInit(): void {
+    this.getGame()
+  }
+  
   constructor(
     private route: ActivatedRoute,
     private gameService: GameService,
     private location: Location
-  ) { }
-
-  ngOnInit(): void {
-    this.getGame()
-  }
-
+  ) { }  
+    
   getGame(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.gameService.getGame(id)
         .subscribe(game => this.game = game)
   }
-
+  
 }
