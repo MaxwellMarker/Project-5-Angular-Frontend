cdfaf086ba0e03674f11ef865de34bf7d14b55fb
Max Marker <maxmarker98@gmail.com>
2020-10-01 23:24:06 -0400

    almost finished with review creation

--- src/app/review.service.ts
+++ src/app/review.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Review } from './review';
+import { server } from './variables';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) {}
+
+  private reviewsUrl = `${server}/reviews`;
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  getReviews(id: string): Observable<Review[]> {
+    const url = `${this.reviewsUrl}/${id}`
+    return this.http.get<Review[]>(url).pipe(
+      catchError(this.handleError<Review[]>(`getGame ${id}`))
+    )
+  }
 }
