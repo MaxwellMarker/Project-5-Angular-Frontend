bae8824437ce9d4821fba50d34f5082260930cdf
Max Marker <maxmarker98@gmail.com>
2020-10-05 04:50:40 -0400

    trying to deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+express = require("express");
+const path = require('path')
+app = express();
+
+app.use(express.static("./dist/project5-frontend"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/project5-frontend/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
