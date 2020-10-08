68b7a9af39c5fde838c35538caa2455a8fce1e93
Max Marker <maxmarker98@gmail.com>
2020-10-07 20:08:46 -0400

    added selected route styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,82 +11,97 @@ import { CoverDirective } from '../cover.directive';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  
+
   games: Game[];
   topGames: Game[];
   recentGames: Game[];
   randomGames: Game[];
   carouselIndex: number;
   carouselCover: string;
-  @ViewChildren(CoverDirective) cover;
   
+  @ViewChildren(CoverDirective) cover;
+
   constructor(
     private route: ActivatedRoute,
     private gameService: GameService,
     private reviewService: ReviewService,
     private location: Location
-    ) { }
-    
-    ngOnInit(): void {
-      this.getGames();
-      this.carouselIndex = 0;
-      this.setCarousel();
-      this.getTopGames();
-      this.getRecentGames();
-      this.getRandomGames();
+  ) {}
+
+  ngOnInit(): void {
+    this.getGames();
+    this.carouselIndex = 0;
+    this.setCarousel();
+    this.getTopGames();
+    this.getRecentGames();
+    this.getRandomGames();
   }
+
   shuffleArray(array): Game[] {
-    for(let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
-    return array
-}
+    return array;
+  }
+
   setCarousel(): void {
     setTimeout(() => {
-      this.cover.first.coverOn()
+      this.cover.first.coverOn();
     }, 5300);
     setTimeout(() => {
-      if(this.carouselIndex === 0){
+      if (this.carouselIndex === 0) {
         this.carouselIndex = 1;
-      }else if(this.carouselIndex === 1){
+      } else if (this.carouselIndex === 1) {
         this.carouselIndex = 6;
-      }else if(this.carouselIndex === 6){
+      } else if (this.carouselIndex === 6) {
         this.carouselIndex = 0;
       }
-      this.cover.first.coverOff()
-      this.setCarousel()
+      this.cover.first.coverOff();
+      this.setCarousel();
     }, 6000);
   }
+
   getGames(): void {
-    this.gameService.getGames()
-        .subscribe(games => this.games = games.sort((a, b) => {
-          if(a.name > b.name){
-            return 1
+    this.gameService.getGames().subscribe(
+      (games) =>
+        (this.games = games.sort((a, b) => {
+          if (a.name > b.name) {
+            return 1;
           }
-          if(b.name > a.name){
-            return -1
+          if (b.name > a.name) {
+            return -1;
           }
-          return 0
-        }));
+          return 0;
+        }))
+    );
   }
+
   getTopGames(): void {
-    this.gameService.getGames()
-        .subscribe(games => this.topGames = games.sort((a, b) => {
-          return b.rating - a.rating
-        }));
+    this.gameService.getGames().subscribe(
+      (games) =>
+        (this.topGames = games.sort((a, b) => {
+          return b.rating - a.rating;
+        }))
+    );
   }
+
   getRecentGames(): void {
-    this.gameService.getGames()
-        .subscribe(games => this.recentGames = games.sort((a, b) => {
-          return Date.parse(b.createdAt) - Date.parse(a.createdAt)
-        }));
+    this.gameService.getGames().subscribe(
+      (games) =>
+        (this.recentGames = games.sort((a, b) => {
+          return Date.parse(b.createdAt) - Date.parse(a.createdAt);
+        }))
+    );
   }
+
   getRandomGames(): void {
-    this.gameService.getGames()
-        .subscribe(games => this.randomGames = this.shuffleArray(games));
+    this.gameService
+      .getGames()
+      .subscribe((games) => (this.randomGames = this.shuffleArray(games)));
   }
+
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location} from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  currentPage: string;
+
+  constructor(
+    private location: Location
+  ) { };
 
   ngOnInit(): void {
-  }
+    this.currentPage = this.location.path();
+  };
+
+  onSelect(page): void {
+    this.currentPage = page;
+  };
 
 }
